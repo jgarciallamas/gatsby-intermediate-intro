1be702212922483af42dc38014f4c9caabffb455
Jose Manuel Garcia Llamas <josemgarciallamas@gmail.com>
2020-03-23 17:42:49 +0100

    feat(theme-dev) createNode added

--- packages/gatsby-theme-docs/gatsby-config.js
+++ packages/gatsby-theme-docs/gatsby-config.js
@@ -1,10 +1,35 @@
-const withDefaults = require('./utils/default-options');
+// const withDefaults = require('./utils/default-options');
 
-// module.exports = ({
-//   basePath = '/',
-//   contentPath = 'docs',
-//   useExternalMDX = false,
-// }) => ({ basePath, contentPath, useExternalMDX });
+// // module.exports = ({
+// //   basePath = '/',
+// //   contentPath = 'docs',
+// //   useExternalMDX = false,
+// // }) => ({ basePath, contentPath, useExternalMDX });
+
+// module.exports = options => {
+//   const { contentPath, useExternalMDX } = withDefaults(options);
+
+//   return {
+//     plugins: [
+//       {
+//         resolve: 'gatsby-source-filesystem',
+//         options: {
+//           name: 'gastby-theme-docs',
+//           path: contentPath,
+//         },
+//       },
+//       !useExternalMDX && {
+//         resolve: 'gatsby-plugin-mdx',
+//         options: {
+//           defaultLayouts: {
+//             default: require.resolve('./src/components/layout.js'),
+//           },
+//         },
+//       },
+//     ].filter(Boolean),
+//   };
+// };
+const withDefaults = require('./utils/default-options');
 
 module.exports = options => {
   const { contentPath, useExternalMDX } = withDefaults(options);
@@ -14,7 +39,7 @@ module.exports = options => {
       {
         resolve: 'gatsby-source-filesystem',
         options: {
-          name: 'gastby-theme-docs',
+          name: 'gatsby-theme-docs',
           path: contentPath,
         },
       },
--- packages/gatsby-theme-docs/gatsby-node.js
+++ packages/gatsby-theme-docs/gatsby-node.js
@@ -23,3 +23,135 @@ exports.onPreBootstrap = ({ store }, options) => {
   // console.log('program.directory ', program.directory);
   // console.log('dir ', dir);
 };
+
+// exports.createSchemaCustomization = ({ actions }) => {
+//   actions.createTypes(`
+//     type DocsPage implements Node @dontInfer {
+//       id: ID!,
+//       title: String!,
+//       path: String!,
+//       date: Date! @dateformat,
+//       body: String!
+//     }
+
+//     `);
+// };
+exports.createSchemaCustomization = ({ actions }) => {
+  console.log('Jose createSchemaCustomization');
+  actions.createTypes(`
+    type DocsPage implements Node @dontInfer {
+      id: ID!
+      title: String!
+      path: String!
+      updated: Date! @dateformat
+      body: String!
+    }
+  `);
+};
+
+// exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+//   const { basePath } = withDefaults(options);
+//   const parent = getNode(node.parent);
+
+//   //we only want mdx files that are loaded by our theme
+//   if (
+//     node.internal.type !== 'Mdx' ||
+//     parent.sourceInstanceName !== 'gatsby-theme-docs'
+//   ) {
+//     return;
+//   }
+
+//   // once we get to here, we know that our node is an Mdx node that was
+//   // loaded by our theme
+
+//   const pageName = parent.name !== 'index' ? parent.name : '';
+
+//   actions.createNode({
+//     id: createNodeId(`DocsPage-${node.id}`),
+//     updated: parent.modifiedTime,
+//     title: node.frontmatter.title || parent.name,
+//     path: path.join('/', basePath, parent.relativeDirectory, pageName),
+//     parent: node.id,
+//     internal: {
+//       type: 'DocsPage',
+//       contentDigest: node.internal.contentDigest,
+//     },
+//   });
+//   // console.log('updated..', parent.modifiedTime);
+//   // console.log('title...', node.frontmatter.title || parent.name);
+//   // console.log(
+//   //   'path...',
+//   //   path.join('/', basePath, parent.relativeDirectory, pageName),
+//   // );
+// };
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+  console.log('Jose onCreateNode', node.internal.type);
+
+  const { basePath } = withDefaults(options);
+  const parent = getNode(node.parent);
+
+  // Only work on MDX files that were loaded by this theme
+  if (
+    node.internal.type !== 'Mdx' ||
+    parent.sourceInstanceName !== 'gatsby-theme-docs'
+  ) {
+    return;
+  }
+
+  // Treat `index.mdx` link `index.html` (i.e. `docs/` vs. `docs/index/`).
+  const pageName = parent.name !== 'index' ? parent.name : '';
+
+  actions.createNode({
+    id: createNodeId(`DocsPage-${node.id}`),
+    title: node.frontmatter.title || parent.name,
+    updated: parent.modifiedTime,
+    path: path.join('/', basePath, parent.relativeDirectory, pageName),
+    parent: node.id,
+    internal: {
+      type: 'DocsPage',
+      contentDigest: node.internal.contentDigest,
+    },
+  });
+};
+
+// exports.createResolvers = ({ createResolvers }) => {
+//   createResolvers({
+//     DocsPage: {
+//       body: {
+//         type: 'String!',
+//         resolve: (source, args, context, info) => {
+//           const type = info.schema.getType('Mdx');
+//           const mdxFields = type.getFields();
+//           const resolver = mdxFields.body.resolve;
+
+//           const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+//           return resolver(mdxNode, args, context, { fieldName: 'body' });
+//         },
+//       },
+//     },
+//   });
+// };
+
+exports.createResolvers = ({ createResolvers }) => {
+  console.log('Jose createResolvers');
+
+  createResolvers({
+    DocsPage: {
+      body: {
+        type: 'String!',
+        resolve: (source, args, context, info) => {
+          // Load the resolver for the `Mdx`type `body` field.
+          const type = info.schema.getType('Mdx');
+          const mdxFields = type.getFields();
+          const resolver = mdxFields.body.resolve;
+
+          const mdxNode = context.nodeModel.getNodeById({ id: source.parent });
+
+          return resolver(mdxNode, args, context, {
+            fieldName: 'body',
+          });
+        },
+      },
+    },
+  });
+};
