96fb1b5bf7bc3b2ee032a744e4c20b5d39d106a0
Jose Manuel Garcia Llamas <josemgarciallamas@gmail.com>
2020-03-24 17:39:25 +0100

    feat(theme-dev): add Theme UI

--- packages/gatsby-theme-docs/gatsby-config.js
+++ packages/gatsby-theme-docs/gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = options => {
           },
         },
       },
+      'gatsby-plugin-theme-ui',
     ].filter(Boolean),
   };
 };
--- packages/gatsby-theme-docs/src/components/docs-page.js
+++ packages/gatsby-theme-docs/src/components/docs-page.js
@@ -1,14 +1,26 @@
-import React from 'react';
+/**@jsx jsx */
+// import { Fragment } from 'react';
+import { jsx } from 'theme-ui';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from './layout';
 
 const DocsPage = ({ page }) => {
-  console.log(page);
+  // console.log(page);
   return (
     <Layout>
       <h1>{page.title}</h1>
       <MDXRenderer>{page.body}</MDXRenderer>
-      <p>This page was updated {page.updated}</p>
+      <p
+        sx={{
+          borderTop: theme => `1px solid ${theme.colors.muted}`,
+          color: 'muted',
+          fontSize: 14,
+          mt: 2,
+          pt: 2,
+        }}
+      >
+        This page was updated {page.updated}
+      </p>
     </Layout>
   );
 };
