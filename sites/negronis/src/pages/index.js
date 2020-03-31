080b68892c1b935cd14478f9216fa7924d6613b9
Jose Manuel Garcia Llamas <josemgarciallamas@gmail.com>
2020-03-31 18:24:17 +0200

    feat(negronis) finish website, theme-docs and theme-events added

--- sites/negronis/src/pages/index.js
+++ sites/negronis/src/pages/index.js
@@ -1,20 +1,43 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 // import React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import Image from 'gatsby-image';
 import Layout from '../components/layout';
 
-const Index = () => (
-  <Layout>
-    <h1>Negronis: A Love Story</h1>
-    <p>
-      A Negroni, while simple, is a beautifully complex cocktail with endless
-      opportunities to experiment with flavors.
-    </p>
-    <Link sx={{ variant: 'button.hollow' }} to="/history">
-      Learn the History
-    </Link>
-  </Layout>
-);
+export const query = graphql`
+  query {
+    image: file(name: { eq: "negroni" }) {
+      cloudinary: childCloudinaryAsset {
+        fluid(transformations: ["ar_18:9", "c_fill", "g_auto:subject"]) {
+          ...CloudinaryAssetFluid
+        }
+      }
+    }
+  }
+`;
+
+const Index = ({ data }) => {
+  console.log(data);
+  return (
+    <Layout>
+      <Image fluid={data.image.cloudinary.fluid} alt="Negroni." />
+      <h1>Negronis: A Love Story</h1>
+      <p>
+        A Negroni, while simple, is a beautifully complex cocktail with endless
+        opportunities to experiment with flavors.
+      </p>
+      <Link sx={{ variant: 'button.primary' }} to="/recipes">
+        See Recipes
+      </Link>
+      <Link sx={{ variant: 'button.hollow', ml: 3 }} to="/history">
+        Learn the History
+      </Link>
+      <Link sx={{ variant: 'button.hollow', ml: 3 }} to="/events">
+        Upcoming Events
+      </Link>
+    </Layout>
+  );
+};
 
 export default Index;
